0409762ea4f202bc95ceedaaeafc50d2546e2f8b
clementmwamba <clementmwamba23@gmail.com>
2023-11-13 18:34:59 +0300

    10

--- code-challenge/src/components/Navbar.js
+++ code-challenge/src/components/Navbar.js
@@ -5,10 +5,17 @@ function Navbar() {
     <div>
           <h1 style={{color: 'blue', border: "solid 2px #0000FF", margin: '20px', textAlign:'center'}}>The Royal Bank of Flatiron</h1>
       
-      <div >
+      <div style={{marginBottom: '10px'}} >
         <input type='text' placeholder='Search your recent transactions ....' />
         <button type='button'>Search</button>
         </div>
+    <span >
+        <input type='date' style={{marginLeft: '50px'}} />
+        <input type='text.' placeholder='Description' style={{marginLeft: '10px'}} />
+        <input type='text' placeholder='Categoty'style={{marginLeft: '10px'}}  />
+        <input type='text' placeholder='Amount' style={{marginLeft: '10px'}} />
+    </span>
+    <button>Add Transaction</button>
 
     </div>
   )

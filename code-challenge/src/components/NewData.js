80776508cfacb3943b00ab9639193c8814bd978b
clementmwamba <clementmwamba23@gmail.com>
2023-11-15 14:38:54 +0300

    17

--- code-challenge/src/components/NewData.js
+++ code-challenge/src/components/NewData.js
@@ -4,7 +4,7 @@ function NewData(props) {
   return (
     <div>
       <form onSubmit={props.handleSubmit}>
-        <input type='date' value={props.date} onChange={props.handleDate} />
+        <input  type='date' value={props.date} onChange={props.handleDate} />
         <input type='text' placeholder='Description' value={props.description} onChange={props.handleDescription} />
         <input type='text' placeholder='Category' value={props.category} onChange={props.handleCategory} />
         <input type='text' placeholder='Amount' value={props.amount} onChange={props.handleAmount} />
